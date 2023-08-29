eeacea7e436d0507c634f83ecf03f99531a70eb6
Melani putri <melaniputri1041@gmail.com>
2023-08-29 09:09:59 +0700

    Add project files.

---
+++ Login/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Login
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Masukan Password : ");
+			string password = Console.ReadLine();
+			if (Validate(password)){
+				Console.WriteLine("Password is valid");
+			}
+			else
+			{
+				Console.WriteLine("Password tidak valid");
+			}
+		}
+
+		static bool Validate(string password)
+		{
+			string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
+			if(Regex.IsMatch(password, pattern))
+			{
+				if(!Regex.IsMatch(password, @"[!@#$]"))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
